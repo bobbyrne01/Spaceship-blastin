90851e7ca838ad5f36022767d2b3e9eff1ac01ac
Robert Byrne <bobbyrne01@gmail.com>
2015-06-19 12:13:35 +0100

    stop ship trail on death

--- js/create.js
+++ js/create.js
@@ -74,7 +74,7 @@ function launchBlueEnemy() {
 }
 
 function create() {
-	
+
 	gameOver = game.add.bitmapText(game.world.centerX, game.world.centerY, 'font', 'GAME OVER!', 80);
 	gameOver.anchor.setTo(0.5, 0.5);
 	gameOver.visible = false;
